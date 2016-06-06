b42d1a7432623a12afe18b9f7bafe489b47e14f1
Hirono Yasuda <hirono@Hirono-no-MacBook-Pro.local>
2016-06-07 04:10:05 +0900

    commit1

---
+++ RSS-READER/ArticleViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RSS-READER
+//
+//  Created by Hirono Yasuda on 2016/06/06.
+//  Copyright © 2016年 hiro yasuda. All rights reserved.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
